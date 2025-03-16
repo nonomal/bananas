aba8465d8618d203f9134d8c8c7b3806dce66ab1
Marco Kellershoff <marco@kellershoff.net>
2025-03-16 17:49:36 +0100

    feat(dx): ugprade packages to latest

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,10 +1,11 @@
+import { mount } from 'svelte'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bulma/css/bulma.min.css'
 import '@sweetalert2/theme-bulma/bulma.min.css'
 import App from './App.svelte'
 import './overrides.css'
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById('app')
 })
 
