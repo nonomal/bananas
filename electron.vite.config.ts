b426105b09e0c7f58171212a6aaab3cccac1106e
Marco Kellershoff <marco@kellershoff.net>
2025-03-16 17:57:59 +0100

    feat(ui): remove bulma, add daisyui

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
@@ -32,6 +33,6 @@ export default defineConfig({
         }
       }
     },
-    plugins: [svelte()]
+    plugins: [svelte(), tailwindcss()]
   }
 })
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,8 +1,7 @@
 import { mount } from 'svelte'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'bulma/css/bulma.min.css'
-import '@sweetalert2/theme-bulma/bulma.min.css'
 import App from './App.svelte'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import './app.css'
 import './overrides.css'
 
 const app = mount(App, {
